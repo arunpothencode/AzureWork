f7373f3d53e9df86b195bc18b1136f03591612b9
Arun R <a4.arun@yahoo.com>
2021-11-09 14:31:02 +0530

    Input changed

--- AzCodeDeployment.ps1
+++ AzCodeDeployment.ps1
@@ -1,91 +1,46 @@
-﻿$comp = Read-Host "Select the component:
-======================
-Press 1 for Pipelines
-Press 2 for Linkedservice
-Press 3 for Dataflow
-Press 4 for Dataset
+﻿$inppara = Get-Content "C:\Users\arun.r\Documents\Sharath\names.txt"| Select-Object -Skip 1
 
-ctrl + c to abort the operation
-======================================>"
-
-
-
-$inppara = Get-Content "C:\Users\arun.r\Documents\Sharath\names.txt"| Select-Object -Skip 1
-
-
-if($comp -eq 1){
-    
-    function ExecPipeline {
+function ExecPipeline {
         param ([string]$PipelineName,[string]$WorkspaceName)
 
-        Get-AzSynapsePipeline -WorkspaceName $WorkspaceName -Name $PipelineName 
+        Get-AzSynapsePipeline -WorkspaceName $WorkspaceName -Name $PipelineName }
         #Set-AzSynapsePipeline -WorkspaceName $WorkspaceName -Name $PipelineName -DefinitionFile "C:\Users\arun.r\Documents\Sharath\StandardPipeline_support_live\pipeline\$PipelineName.json"
 
-                          }
+foreach($p in $inppara){
+    if($p.split(',')[1] -eq 'pipeline'){
+    &ExecPipeline $p.split(',')[0] $p.split(',')[2]
+}}
 
-    foreach($i in $inppara){
-        if($i.split(',')[1] -eq 'pipeline'){
-        &ExecPipeline $i.split(',')[0] $i.split(',')[2]
-                                           }
-
-                           }continue
-
-
-                }
-elseif($comp -eq 2){
-
-
-    function ExecLinkedService {
-        param ([string]$LinkedName,[string]$WorkspaceName)
-
-        Get-AzSynapseLinkedService -WorkspaceName $WorkspaceName -Name $LinkedName 
-        #Set-AzSynapsePipeline -WorkspaceName $WorkspaceName -Name $PipelineName -DefinitionFile "C:\Users\arun.r\Documents\Sharath\StandardPipeline_support_live\pipeline\$PipelineName.json"
-
-                               }
-                     
-    foreach($i in $inppara){
-        if($i.split(',')[1] -eq 'linkedservice'){
-        &ExecLinkedService $i.split(',')[0] $i.split(',')[2]
-                                                }
-
-                           }continue
-
-                   }
-elseif($comp -eq 3){
-
-
-    function ExecDataflow {
-        param ([string]$DataflowName,[string]$WorkspaceName)
-
-        Get-AzSynapseDataFlow -WorkspaceName $WorkspaceName -Name $DataflowName 
-        #Set-AzSynapsePipeline -WorkspaceName $WorkspaceName -Name $PipelineName -DefinitionFile "C:\Users\arun.r\Documents\Sharath\StandardPipeline_support_live\pipeline\$PipelineName.json"
-
-                               }
-                     
-    foreach($i in $inppara){
-        if($i.split(',')[1] -eq 'dataflow'){
-        &ExecDataflow $i.split(',')[0] $i.split(',')[2]
-                                                }
-
-                           }continue
-
-                   }
-elseif($comp -eq 4){
-
-
-    function ExecDataset {
+function ExecDataset {
         param ([string]$DatasetName,[string]$WorkspaceName)
 
-        Get-AzSynapseDataset -WorkspaceName $WorkspaceName -Name $DatasetName 
-        #Set-AzSynapseDataset -WorkspaceName $WorkspaceName -Name $PipelineName -DefinitionFile "C:\Users\arun.r\Documents\Sharath\StandardPipeline_support_live\pipeline\$PipelineName.json"
+        Get-AzSynapseDataset -WorkspaceName $WorkspaceName -Name $DatasetName }
+        #Set-AzSynapseDataset -WorkspaceName $WorkspaceName -Name $DatasetName -DefinitionFile "C:\Users\arun.r\Documents\Sharath\StandardPipeline_support_live\dataset\$DatasetName.json"
 
-                               }
-                     
-    foreach($i in $inppara){
-        if($i.split(',')[1] -eq 'dataset'){
-        &ExecDataset $i.split(',')[0] $i.split(',')[2]
-                                                }
 
-                           }continue
+foreach($d in $inppara){
+    if($d.split(',')[1] -eq 'dataset'){
+    &ExecDataset $d.split(',')[0] $d.split(',')[2]
+}}
 
-                   }
+function ExecLinked {
+        param ([string]$LinkedName,[string]$WorkspaceName)
+
+        Get-AzSynapseLinkedService -WorkspaceName $WorkspaceName -Name $LinkedName }
+        #Set-AzSynapseLinkedService -WorkspaceName $WorkspaceName -Name $LinkedName -DefinitionFile "C:\Users\arun.r\Documents\Sharath\StandardPipeline_support_live\linkedservice\$LinkedName.json"
+
+foreach($l in $inppara){
+    if($l.split(',')[1] -eq 'linkedservice'){
+    &ExecLinked $l.split(',')[0] $l.split(',')[2]
+}}
+
+function ExecDataflow {
+        param ([string]$FlowName,[string]$WorkspaceName)
+
+        Get-AzSynapseDataFlow -WorkspaceName $WorkspaceName -Name $FlowName }
+        #Set-AzSynapseDataFlow -WorkspaceName $WorkspaceName -Name $FlowName -DefinitionFile "C:\Users\arun.r\Documents\Sharath\StandardPipeline_support_live\dataflow\$FlowName.json"
+
+foreach($f in $inppara){
+    if($f.split(',')[1] -eq 'dataflow'){
+    &ExecDataflow $f.split(',')[0] $f.split(',')[2]
+}}
